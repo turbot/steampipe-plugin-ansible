304729070219d88a20c567a4f7d85ccd2fa55172
Subhajit Mondal <subhajit@turbot.com>
2023-06-27 11:15:24 +0530

    Initial commit

---
+++ ansible/connection_config.go
@@ -0,0 +1,30 @@
+package ansible
+
+import (
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
+)
+
+type ansibleConfig struct {
+	Paths []string `cty:"paths" steampipe:"watch"`
+}
+
+var ConfigSchema = map[string]*schema.Attribute{
+	"paths": {
+		Type: schema.TypeList,
+		Elem: &schema.Attribute{Type: schema.TypeString},
+	},
+}
+
+func ConfigInstance() interface{} {
+	return &ansibleConfig{}
+}
+
+// GetConfig :: retrieve and cast connection config from query data
+func GetConfig(connection *plugin.Connection) ansibleConfig {
+	if connection == nil || connection.Config == nil {
+		return ansibleConfig{}
+	}
+	config, _ := connection.Config.(ansibleConfig)
+	return config
+}
---
+++ ansible/plugin.go
@@ -0,0 +1,25 @@
+package ansible
+
+import (
+	"context"
+
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+)
+
+const pluginName = "steampipe-plugin-ansible"
+
+// Plugin creates this (ansible) plugin
+func Plugin(ctx context.Context) *plugin.Plugin {
+	p := &plugin.Plugin{
+		Name:             pluginName,
+		DefaultTransform: transform.FromCamel().NullIfZero(),
+		ConnectionConfigSchema: &plugin.ConnectionConfigSchema{
+			NewInstance: ConfigInstance,
+			Schema:      ConfigSchema,
+		},
+		TableMap: map[string]*plugin.Table{},
+	}
+
+	return p
+}
